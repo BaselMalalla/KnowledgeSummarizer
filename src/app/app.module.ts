8306fd88a5ee2e105e4cc40d7ea6ddac4c29d92d
Basel Malalla <malalla.basel@gmail.com>
2024-05-18 21:23:35 +0300

    Initial commit with recycled project

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { ReactiveFormsModule } from '@angular/forms';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAv_oEZf9avWn5_aTvW_t34XdnkGGpsveM',
+  authDomain: 'knowledgesummarizer-2802b.firebaseapp.com',
+  projectId: 'knowledgesummarizer-2802b',
+  storageBucket: 'knowledgesummarizer-2802b.appspot.com',
+  messagingSenderId: '782921536535',
+  appId: '1:782921536535:web:4f9df074fa0b513e87f852',
+  measurementId: 'G-VR1KP844C9',
+};
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    ReactiveFormsModule,
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
